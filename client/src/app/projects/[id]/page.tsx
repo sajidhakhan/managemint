363124dc44a8dddb209a7a330969702df2ac5f4f
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-09-18 17:43:26 +0530

    Timeline view added

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import ProjectHeader from '@/app/projects/ProjectHeader';
 import Board from '../BoardView';
 import List from '../ListView';
+import Timeline from '../TimelineView';
 
 type Props = { 
     params: {id: string};
@@ -22,6 +23,9 @@ const page = ({ params }: Props) => {
         )}
         {activeTab === "List" && (
              <List id={id} setIsModalNewTaskOpen= {setIsModalNewTaskOpen} />
+        )}
+         {activeTab === "Timeline" && (
+             <Timeline id={id} setIsModalNewTaskOpen= {setIsModalNewTaskOpen} />
         )}
     </div>
   )
