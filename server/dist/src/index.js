552820815a294a66e498c15e7a5965b865e0fcbc
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-09-19 16:09:42 +0530

    users page added

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -12,6 +12,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
 const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
 const searchRoutes_1 = __importDefault(require("./routes/searchRoutes"));
+const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
@@ -27,6 +28,7 @@ app.get("/", (req, res) => {
 app.use("/projects", projectRoutes_1.default);
 app.use("/tasks", taskRoutes_1.default);
 app.use("/search", searchRoutes_1.default);
+app.use("/users", userRoutes_1.default);
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 import projectRoutes from './routes/projectRoutes';
 import taskRoutes from './routes/taskRoutes';
 import searchRoutes from './routes/searchRoutes';
+import userRoutes from './routes/userRoutes';
 
 dotenv.config();
 const app = express();
@@ -25,6 +26,7 @@ app.get("/", (req,res) =>{
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
+app.use("/users", userRoutes);
 
 const port = process.env.PORT || 3000 ;
 app.listen(port, () =>{
