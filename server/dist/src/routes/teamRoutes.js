9a68da435cb474ba52efa8a990653a578379f97f
sajidhakhan <81672634+sajidhakhan@users.noreply.github.com>
2024-09-22 18:17:30 +0530

    updated gitignore file

--- server/dist/src/routes/teamRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const teamController_1 = require("../controllers/teamController");
-const router = (0, express_1.Router)();
-router.get("/", teamController_1.getTeams);
-exports.default = router;
